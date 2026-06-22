d01470cefd977aa2e7765fb4cea558c73e887633
Hussein Abuammar <husseinabuammar24@gmail.com>
2026-06-22 15:01:42 +0200

    Update cleanData.py
    fix types

--- Data/cleanData.py
+++ Data/cleanData.py
@@ -12,6 +12,37 @@ df["street"] = df["street"].str.lower()
 df[~df["livable_surface"].isna()]["livable_surface"].astype(int)
 
 # 3. fix longitude/latitude that are swapped
+# --- CATEGORICAL ---
+cat_cols = ['transaction_type', 'province', 'property_type', 
+            'property_subtype', 'property_condition']
+for col in cat_cols:
+    df[col] = pd.Categorical(df[col])
+
+# --- INTEGER ---
+int_cols = ['price', 'date_of_construction', 'land_surface', 'livable_surface',
+            'energy_consumption', 'number_of_bedrooms', 'number_of_bathrooms',
+            'garage', 'garden', 'terrace', 'postal_code', 'street_number', 'seller_id']
+for col in int_cols:
+    df[col] = pd.to_numeric(df[col], errors='coerce').astype('Int64')  # Int64 handles NaN
+
+# --- BOOLEAN ---
+bool_cols = ['elevator', 'swimming_pool', 'balcony', 'furnished']
+for col in bool_cols:
+    df[col] = df[col].astype('boolean')  # nullable boolean, handles NaN
+
+# --- FLOAT ---
+float_cols = ['latitude', 'longitude']
+for col in float_cols:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
+# --- DATE ---
+#df['availability'] = pd.to_datetime(df['availability'], errors='coerce')
+
+# --- STRING ---
+str_cols = ['street']
+for col in str_cols:
+    df[col] = df[col].astype('string')
+
 
 
 # 4. remove data with values out of bounds
